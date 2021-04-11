47bff634e5fd5e6f23f773ff3a4c6470f27a16b1
espinhara <gabrielespinhara13@gmail.com>
2021-04-11 00:10:59 -0300

    implements utils with model users

--- utils.py
+++ utils.py
@@ -1,10 +1,24 @@
-from models import Person
+from models import Person, Users
 
 
 def insert_person():
     person = Person(name="Ricardo", age=50)
     print(person)
     person.save()
+    person = Person(name="Gabriel", age=20)
+    print(person)
+    person.save()
+
+
+def insert_user():
+    user = Users(login="espinhara", password="123")
+    user.save()
+    print(user)
+
+
+def query_all_users():
+    user = Users.query.all()
+    print(user)
 
 
 def query_all_person():
@@ -31,6 +45,8 @@ def delete_person():
 if __name__ == '__main__':
     # insert_person()
     # alter_person()
-    delete_person()
-    query_all_person()
+    # delete_person()
+    # insert_user()
+    # query_all_person()
+    query_all_users()
     # query_person()
